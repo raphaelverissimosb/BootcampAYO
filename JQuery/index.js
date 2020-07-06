02614ff996070fd11b97fb7ed9b4dcffc58ffa86
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-07-06 17:56:15 -0300

    using my first jquery commands

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,1 +1,13 @@
-$("h1").css("color", "red");
+// $(document).ready(function(){
+//     $("h1").css("color", "red");    
+// });
+
+//Jquery method
+$("h1").text("Bye Vue !");
+$("button").html("<em>Hey</em>")
+// $("button");
+$("img").attr("src")
+
+//Js Method
+// document.querySelector("h1");
+// document.querySelectorAll("button");
